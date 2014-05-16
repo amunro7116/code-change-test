752693cf7f8b4f860a40968ef2cdc3cf26f7a0eb
amunro7116 <amunro7116@sohamcollege.org.uk>
2014-05-16 15:42:05 +0100

    my update
    I made the grid using a turtle innstead of printing and as im typing this comment i have though of a better way of doing it

--- grid.py
+++ grid.py
@@ -1,4 +1,206 @@
-print("-------")
-print("| | | |")
-print("-------")
-print("| | | |")
+import turtle
+pane=turtle.Screen()
+lel= turtle.Turtle()
+lel.penup()
+lel.backward(200)
+lel.left(90)
+lel.forward(200)
+lel.right(90)
+lel.pendown()
+def grid(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid(100)
+lel.forward(100)
+def grid1(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid1(100)
+lel.forward(100)
+def grid2(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid2(100)
+lel.forward(100)
+def grid3(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid3(100)
+lel.forward(100)
+def grid4(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid4(100)
+lel.backward(400)
+lel.right(90)
+lel.forward(100)
+lel.left(90)
+def grid5(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid5(100)
+lel.forward(100)
+def grid6(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid6(100)
+lel.forward(100)
+def grid7(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid7(100)
+lel.forward(100)
+def grid8(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid8(100)
+lel.forward(100)
+def grid9(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid9(100)
+lel.backward(400)
+lel.right(90)
+lel.forward(100)
+lel.left(90)
+def grid10(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid5(100)
+lel.forward(100)
+def grid11(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid6(100)
+lel.forward(100)
+def grid12(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid7(100)
+lel.forward(100)
+def grid13(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid8(100)
+lel.forward(100)
+def grid14(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid9(100)
+lel.backward(400)
+lel.right(90)
+lel.forward(100)
+lel.left(90)
+def grid15(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid5(100)
+lel.forward(100)
+def grid16(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid6(100)
+lel.forward(100)
+def grid17(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid7(100)
+lel.forward(100)
+def grid18(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid8(100)
+lel.forward(100)
+def grid19(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid9(100)
+lel.backward(400)
+lel.right(90)
+lel.forward(100)
+lel.left(90)
+def grid20(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid5(100)
+lel.forward(100)
+def grid21(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid6(100)
+lel.forward(100)
+def grid22(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid7(100)
+lel.forward(100)
+def grid23(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid8(100)
+lel.forward(100)
+def grid24(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid9(100)
+lel.backward(400)
+lel.right(90)
+lel.forward(100)
+lel.left(90)
+def grid25(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid5(100)
+lel.forward(100)
+def grid26(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid6(100)
+lel.forward(100)
+def grid27(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid7(100)
+lel.forward(100)
+def grid28(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid8(100)
+lel.forward(100)
+def grid29(size):
+    for x in range(4):
+        lel.forward(size)
+        lel.right(90)
+grid9(100)
+
+
+
